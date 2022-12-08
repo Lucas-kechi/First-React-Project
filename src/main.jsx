fac63b8fda194edfdc493b86aaec027845975567
A81435269g <lukaskechi7@gmail.com>
2022-12-08 15:45:34 -0300

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Move from './pages/home'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Move />
+  </React.StrictMode>
+)
---
+++ src/pages/home.jsx
@@ -0,0 +1,7 @@
+function home() {
+  return (
+    <h1>hello world!</h1>
+  )
+}
+
+export default home
