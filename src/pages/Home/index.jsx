2e380ece4b97460f336e0ba4fd1ee06b9864684b
A81435269g <lukaskechi7@gmail.com>
2022-12-08 17:03:15 -0300

    Organizando páginas da aplicação

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
-function home() {
+import './style.css';
+
+export function Home() {
   return ( // abaixo teremos o retorno da função, ela deve retornar apenas um elemento, em resolução aqui usamos uma <div> mas também podemos utilizar um 'fragment', um elemento vazio, <> (conteúdo HTML) </>
     <div> 
       <h1>hello world!</h1>
@@ -7,5 +9,3 @@ function home() {
   
   )
 }
-
-export default home
