6970dabd5690a5ce1d99d31084760f44d970522e
A81435269g <lukaskechi7@gmail.com>
2022-12-12 17:54:40 -0300

    Adicionando css A nosso page home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,9 +2,10 @@ import './style.css';
 
 export function Home() {
   return ( // abaixo teremos o retorno da função, ela deve retornar apenas um elemento, em resolução aqui usamos uma <div> mas também podemos utilizar um 'fragment', um elemento vazio, <> (conteúdo HTML) </>
-    <div> 
-      <h1>hello world!</h1>
-      <p> lucas</p>
+    <div className='container'> 
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder='Digite o nome...' />
+      <button type='button'>Adicionar</button>
     </div>
   
   )
