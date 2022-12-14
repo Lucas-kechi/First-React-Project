53f56e6a80097243461ac3be4c9dddbe74a5a9b9
A81435269g <lukaskechi7@gmail.com>
2022-12-14 15:58:25 -0300

    Acrescentando um Estado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,19 @@
+import React, { useState } from 'react';
 import './style.css';
 import { Card } from '../../components/Card';
 
 export function Home() {
+  const [personName, setPersonName] = useState() //Aqui também podemos deixar um informação como default ex: useState('Adriana')
+
   return ( // abaixo teremos o retorno da função, ela deve retornar apenas um elemento, em resolução aqui usamos uma <div> mas também podemos utilizar um 'fragment', um elemento vazio, <> (conteúdo HTML) </>
     <div className='container'> 
       <h1>Lista de Presença</h1>
-      <input type="text" placeholder='Digite o nome...' />
+      <h2> Nome: {personName}</h2>
+      <input 
+        type="text" 
+        placeholder='Digite o nome...' 
+        onChange={e => setPersonName(e.target.value)}
+      />
       <button type='button'>Adicionar</button>
       
       <div className='list'>
